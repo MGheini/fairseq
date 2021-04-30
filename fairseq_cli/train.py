968a1cc3fc93cfe34e12b5d925fe251db3e05fd8
Mozhdeh Gheini <gheini72@gmail.com>
2021-04-29 18:24:24 -0700

    Fix typo

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -375,7 +375,7 @@ def main(args):
         logger.info(f'loading the trimmed mbart from '
                     f'{args.finetune_from_mbart_at}')
         pretrained_state_dict = torch.load(args.finetune_from_mbart_at)['model']
-        pretrained_state_dict = model.upgrade_state_dict_named(pretrained_state_dict)
+        pretrained_state_dict = model.upgrade_state_dict_named(pretrained_state_dict, '')
         model_params = set()
         for name, _ in model.named_parameters():
             model_params.add(name)
